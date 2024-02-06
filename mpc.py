457388dd530dce485db648bdecf4698e41e8ce41
shivakumar-tekumatla <shivakumartekumatla@gmail.com>
2024-02-05 21:55:21 -0500

    Change object function

--- mpc.py
+++ mpc.py
@@ -12,14 +12,20 @@ class MPC(Robot):
     def __init__(self,parameters,num_obstacles=3):
         super().__init__()
         self.robot_parameters = parameters["Robot"]
+        
+        # these are all the states of the robot at a given instance 
         self.robot_position = np.array(self.robot_parameters["Start"])
-        self.Torque_MIN = self.robot_parameters["MIN Torque"]
-        self.Torque_MAX = self.robot_parameters["MAX Torque"]
         self.robot_velocity = np.array([0,0])
         self.theta = self.robot_parameters["Theta"] # initial heading angle is zero 
         self.theta_d = self.robot_parameters["Theta_d"] # inital heading angular velocity is zero 
         self.wheel_angles = self.robot_parameters["Wheel_angles"]
         self.wheel_velocities = self.robot_parameters["Wheel_velocities"]
+        
+        # config parameters 
+        self.Torque_MIN = self.robot_parameters["MIN Torque"]
+        self.Torque_MAX = self.robot_parameters["MAX Torque"]
+        
+        # mpc parameters 
         self.mpc_parameters = parameters["MPC"]
         self.dt = self.mpc_parameters["dt"]
         self.sim_time = self.mpc_parameters["sim_time"]
@@ -31,12 +37,10 @@ class MPC(Robot):
         self.obstacle_bounding_size = self.mpc_parameters["obstacle_bounding_size"] # assuming the overall shape of obstacle as square 
         self.HORIZON  = self.mpc_parameters["Horizon"]
         self.STEP = self.mpc_parameters["Step"]
+        
         self.robot_bound = self.robot_parameters["Bounding_Size"]
         self.num_obstacles = num_obstacles
-        # bound = np.array([(self.Torque_MIN,self.Torque_MAX)]) #,[(self.Torque_MIN,self.Torque_MAX)]
-        self.bounds = [(self.Torque_MIN,self.Torque_MAX) for a in range(2)]#*self.HORIZON)]
-        print("Bounds.........!",self.bounds)
-        
+        self.bounds = [(self.Torque_MIN,self.Torque_MAX) for a in range(2*self.HORIZON)]
         return
     
     def cost(self,robot_positions,trajectory_positions,obstacles_positions,obs_optim=False):
@@ -59,34 +63,68 @@ class MPC(Robot):
         return tracking_error+collision_error #collision_error#
     
     def object_function(self,control_inputs):
+        # unpacking states  
         x,x_dot,y,y_dot,theta,theta_dot,phi1,phi1_dot,phi2,phi2_dot = self.states 
-
+        # creating empty arrays for robot positions, trajectory positions and obstacle positions
+        robot_positions      = np.empty((0,2)) 
         trajectory_positions = np.empty((0,2))
-        obstacles_positions = np.empty((0,2))
+        obstacles_positions  = np.empty((0,2))
+        
+         
+        # current robot position and velocity 
         robot_position = np.array([x,y]) #self.robot_position
         robot_velocity = np.array([x_dot,y_dot]) #self.robot_velocity
+        # current wheels' velocities and angles 
         wheels_velocities = np.array([phi1_dot,phi2_dot])#self.wheel_velocities
         wheels_angles = np.array([phi1,phi2])#self.wheel_angles
-        trajectory_position = self.trajectory_position # current desired position 
+        
+        #current trajectory details 
+        trajectory_position = self.trajectory_position  # current desired position 
         trajectory_velocity = self.trajectory_velocity  # current desired velocity
+        # current obstacle details         
         obstacles_position = self.obstacles_position 
         obstacles_velocity = self.obstacles_velocity
+        
+        
+        
+        robot_positions = np.append(robot_positions, [robot_position], axis=0)
         trajectory_positions = np.append(trajectory_positions, [trajectory_position], axis=0)
         obstacles_positions = np.append(obstacles_positions, [obstacles_position], axis=0)
+        
+        for i in range(self.HORIZON): 
+            t = np.linspace(0,self.STEP,2)  
+            states = self.simulate_dynamics(t,robot_position,robot_velocity,theta,theta_dot,wheels_angles,wheels_velocities,control_inputs[i:i+2]) # these are the robot_states in the future 
 
-        # with these control inputs , what are the robot's next states?
-        t  = np.linspace(0,self.HORIZON*self.STEP,self.HORIZON+1)  # simulate for this time for  
-        states = self.simulate_dynamics(t,robot_position,robot_velocity,theta,theta_dot,wheels_angles,wheels_velocities,control_inputs) # these are the robot_states in the future 
-        # compute the error for this 
-        x,x_dot,y,y_dot,theta,theta_dot,phi1,phi1_dot,phi2,phi2_dot = states.T 
-        robot_positions = np.vstack((x,y)).T
-        robot_velocities = np.vstack((x_dot,y_dot)).T
-        # predict the trajectory
-        for i in range(self.HORIZON):
+            # future robot states 
+            x,x_dot,y,y_dot,theta,theta_dot,phi1,phi1_dot,phi2,phi2_dot = states.T 
+            robot_position = np.array([x[-1],y[-1]]) # last state from the simulation is going to be the next state 
+            robot_velocity = np.array([x_dot[-1],y_dot[-1]]) 
+            theta = theta[-1]
+            theta_dot = theta_dot[-1]
+            wheels_velocities = np.array([phi1_dot[-1],phi2_dot[-1]])#self.wheel_velocities
+            wheels_angles = np.array([phi1[-1],phi2[-1]])#self.wheel_angles
+                        
+            robot_positions = np.append(robot_positions, [robot_position], axis=0)
+            # future trajectory and obstacle predictions 
+            #TODO : use nonlinear model to predict the future trajectory positions and obstacle positions 
             trajectory_position = trajectory_position + trajectory_velocity*self.STEP
             trajectory_positions = np.append(trajectory_positions, [trajectory_position], axis=0)
             obstacles_position = obstacles_position + obstacles_velocity*self.STEP
             obstacles_positions = np.append(obstacles_positions, [obstacles_position], axis=0)
+        
+        # # with these control inputs , what are the robot's next states?
+        # t  = np.linspace(0,self.HORIZON*self.STEP,self.HORIZON+1)  # simulate for this time for  
+        # states = self.simulate_dynamics(t,robot_position,robot_velocity,theta,theta_dot,wheels_angles,wheels_velocities,control_inputs) # these are the robot_states in the future 
+        # # compute the error for this 
+        # x,x_dot,y,y_dot,theta,theta_dot,phi1,phi1_dot,phi2,phi2_dot = states.T 
+        # robot_positions = np.vstack((x,y)).T
+        # robot_velocities = np.vstack((x_dot,y_dot)).T
+        # # predict the trajectory
+        # for i in range(self.HORIZON):
+        #     trajectory_position = trajectory_position + trajectory_velocity*self.STEP
+        #     trajectory_positions = np.append(trajectory_positions, [trajectory_position], axis=0)
+        #     obstacles_position = obstacles_position + obstacles_velocity*self.STEP
+        #     obstacles_positions = np.append(obstacles_positions, [obstacles_position], axis=0)
         cost_ = self.cost(robot_positions,trajectory_positions,obstacles_positions)
         # print("Cost...",cost,end="\r")
         return cost_
@@ -98,8 +136,11 @@ class MPC(Robot):
         self.trajectory_velocity = trajectory_velocity
         self.obstacles_position = obstacles_position
         self.obstacles_velocity = obstacles_velocity
+        inputs = []
+        [inputs.extend([MPC.current_inputs[0],MPC.current_inputs[1]]) for i in range(self.HORIZON)]  # Defining initial inputs same as the current inputs 
+        inputs = np.array(inputs)
 
-        res = minimize(self.object_function, MPC.current_inputs, method='SLSQP', bounds= self.bounds)
+        res = minimize(self.object_function, inputs, method='SLSQP', bounds= self.bounds)
         # print("Optimizer result ",res,end="\r")
         best_inputs = np.array([res.x[0],res.x[1]])
         MPC.current_inputs = best_inputs
@@ -213,7 +254,7 @@ def main():
         obstacles_position = obstacle_positions[i]
         obstacles_velocity = (obstacle_positions[i+1]-obstacles_position)/mpc.dt #np.array([obstacle_velocities[i]])
         inputs = mpc.control(states,trajectory_position,trajectory_velocity,obstacles_position,obstacles_velocity)#,obstacle_cost_optim)
-
+        print(inputs)
         print(f"Time {sim_time[i]} Control Inputs {inputs}", end="\r")
 
         states_ = mpc.simulate_dynamics(t,robot_position,robot_velocity,theta,theta_dot,wheel_angles,wheel_velocities,inputs) # these are the robot_states in the future 
